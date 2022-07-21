8e82dcaa788c2beb02b64291d5c7f862f082409b
Irfan <irfanimanabub@gmail.com>
2022-07-21 10:55:40 +0800

    added style.css and app properties

--- src/main/java/sg/edu/nus/iss/d13revision/models/PersonForm.java
+++ src/main/java/sg/edu/nus/iss/d13revision/models/PersonForm.java
@@ -2,9 +2,11 @@ package sg.edu.nus.iss.d13revision.models;
 
 public class PersonForm {
 
+    // this class handles the form 
+    
     private String firstName;
     private String lastName;
-    
+
     public String getFirstName() {
         return firstName;
     }
